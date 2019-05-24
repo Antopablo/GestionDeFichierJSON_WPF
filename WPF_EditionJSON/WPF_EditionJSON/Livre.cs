7d4d639759fb1afecca184921189e448f76b5982
Antony LEFEVRE <34274907+Antopablo@users.noreply.github.com>
2019-05-24 09:43:56 +0200

    Add files via upload

---
+++ WPF_EditionJSON/WPF_EditionJSON/Livre.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_EditionJSON
+{
+    [DataContract]
+    class Livre : INotifyPropertyChanged
+    {
+        private string _resume;
+        private DateTime _anneeSortie;
+        private string _auteur;
+        private string _titre;
+
+        public Livre(string titre,  string auteur)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            Resume = "Lorem ipsum dolor sit amet";
+        }
+        public Livre()
+        {
+            Resume = "Lorem ipsum dolor sit amet";
+        }
+
+        [DataMember(Order = 3)]
+        public string Resume
+        {
+            get { return _resume; }
+            set {
+                if (this._resume != value)
+                {
+                    this._resume = value;
+                    this.NotifyPropertyChanged("Resume");
+                }
+            }
+        }
+
+        [DataMember(Order = 0)]
+        public string Titre
+        {
+            get { return _titre; }
+            set {
+                if (this._titre != value)
+                {
+                    this._titre = value;
+                    this.NotifyPropertyChanged("Titre");
+                }
+            }
+        }
+
+        [DataMember(Order = 2)]
+        public DateTime AnneeSortie
+        {
+            get { return _anneeSortie; }
+            set {
+                if (this._anneeSortie != value)
+                {
+                    this._anneeSortie = value;
+                    this.NotifyPropertyChanged("AnneeSortie");
+                }
+            }
+        }
+
+        [DataMember(Order = 1)]
+        public string Auteur
+        {
+            get { return _auteur; }
+            set {
+                if (this._auteur != value)
+                {
+                    this._auteur = value;
+                    this.NotifyPropertyChanged("Auteur");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
---
+++ WPF_EditionJSON/WPF_EditionJSON/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace WPF_EditionJSON
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Livre> Collection_livre = new ObservableCollection<Livre>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Data_Grid.ItemsSource = Collection_livre;
+            Data_Grid.IsReadOnly = true;
+        }
+
+        private void ChargerJSON_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "JSON Files | *.json";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(ObservableCollection<Livre>));
+                Stream stm = new StreamReader(openFileDialog.FileNames[0], true).BaseStream;
+                ObservableCollection<Livre> tmp = (ObservableCollection<Livre>)ser.ReadObject(stm);
+                stm.Close();
+                Collection_livre.Clear();
+                foreach (Livre item in tmp)
+                {
+                    Collection_livre.Add(item);
+                }
+            }
+        }
+
+        private void SauvJSON_Click(object sender, RoutedEventArgs e)
+        {
+                Stream stm = new StreamWriter("Save.json").BaseStream;
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(ObservableCollection<Livre>));
+                ser.WriteObject(stm, Collection_livre);
+                stm.Close();
+        }
+
+        private void EditJSON_Click(object sender, RoutedEventArgs e)
+        {
+            Data_Grid.IsReadOnly = false;
+            Data_Grid.BeginEdit();
+        }
+
+      
+    }
+}
